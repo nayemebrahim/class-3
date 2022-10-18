f925014c55608cde26b5ba97e383314a8faf0126
Nayem <89932140+nayemebrahim@users.noreply.github.com>
2022-10-18 23:12:40 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// var mySong = document.getElementById("mySong");
+// var icon = document.getElementById("icon");
+
+// icon.onclick = function(){
+//     if(mySong.paused){
+//         mySong.play();
+//         icon.src = "pause2.png";
+//         URL
+//     }
+//     else{
+//         mySong.pause();
+//         icon.src = "play.png";
+//     }
+// }
+
+function gotoLink(link) {
+    console.log(link.value);
+    location.href = 'https://www.youtube.com/watch?v=fjUGC8g4GOE'
+}
